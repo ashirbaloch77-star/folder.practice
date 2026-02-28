ee5acc7c1398fec89cf18c9ca7f47970e83e9c9a
ashirbaloch77-star <ashirbaloch77@gmail.com>
2026-02-28 21:59:07 +0500

    add

--- app.js
+++ app.js
@@ -1,11 +1,1 @@
-const express = require("express");
-
-const app = express();
-
-app.get("/", (req, res) => {
-  res.send("Server Working ");
-});
-
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
-});
+alert('welcome to my website');
